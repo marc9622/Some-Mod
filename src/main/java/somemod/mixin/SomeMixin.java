d7a43476c0dfe6340c8aa780cfdbb76cca63456f
marc9622 <72046580+marc9622@users.noreply.github.com>
2023-01-15 03:04:36 +0100

    folder renaming

--- src/main/java/somemod/mixin/SomeMixin.java
+++ src/main/java/somemod/mixin/SomeMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class SomeMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		SomeMod.LOGGER.info("This line is printed by an example mod mixin!");
