a03df1142b67413365749d6e7a4f3bcb1b8f9712
marc9622 <maaw@itu.dk>
2024-06-16 20:41:13 +0200

    Fixed some gl linking on NixOS

--- shell.nix
+++ shell.nix
@@ -6,10 +6,13 @@ in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     flite
+    glfw
+    libglvnd
   ];
 
   shellHook = ''
     export GRADLE_OPTS="-Xmx${gradleCli} -Dorg.gradle.jvmargs='-Xmx${gradleDaemon}'"
     export PATH="$PATH:${pkgs.flite}/lib"
+    export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:}${pkgs.libglvnd}/lib"
   '';
 }
