ffa73d0751c54907be5a56793e29cd3e35b65228
marc9622 <maaw@itu.dk>
2024-06-17 18:35:50 +0200

    Mixin for adding attributes to LivingEntity

---
+++ src/main/java/somemod/mixin/LivingEntityAttributes.java
@@ -0,0 +1,24 @@
+package somemod.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import somemod.frost.entity.attribute.FrostEntityAttributes;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityAttributes {
+
+    @Inject(
+        method = "createLivingAttributes(" +
+                 ")Lnet/minecraft/entity/attribute/DefaultAttributeContainer/Builder;",
+        at = @At("RETURN"),
+        cancellable = false)
+    private void createLivingAttributes(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
+        cir.setReturnValue(cir.getReturnValue().add(FrostEntityAttributes.WARMTH));
+    }
+
+}
